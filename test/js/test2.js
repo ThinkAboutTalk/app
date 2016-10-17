f4892cdd719194912e05672357fc808bb55bc67d
jianguiweilai <497894863@qq.com>
2016-10-17 21:04:26 +0800

    啊手动阀手动阀撒旦发

--- test/js/test2.js
+++ test/js/test2.js
@@ -1,11 +1,10 @@
 
 window.addEventListener("load",function(){
 	
-	
 	var winW = document.body.clientWidth ;
 	
 	var maxWidth = 1000 ; // 默认宽度
-	var marginX = 20 ; 
+	var marginX = 20 ;
 	var marginY = 120 ;
 	var marginToP = 50 ;
 	var fontSize = 30 ;
@@ -23,13 +22,12 @@ window.addEventListener("load",function(){
 		}
 		
 		function getPartPoint(){
-			var treeData = [];
+			
 			data.forEach(function(value,index){
 				var _id = value['idNo'];
 				var _parentId = value['FH'];
 				var _title =  value['expression'];
 				var point = {
-					thispoint : {
 						id : _id ,
 						title : _title,
 						left : 0 ,
@@ -37,43 +35,56 @@ window.addEventListener("load",function(){
 						width : _title.length * fontWidth + pointMargin ,
 						height : piceOneHeight ,
 						parentId : _parentId ,
-						heightAdded : false
-					},
-					parent : {}
+						heightAdded : false,
+						sonIds : []
 				}
 				if(_parentId==""){ // 顶元素
-					point.thispoint.left = maxWidth/2 ;
-					point.thispoint.top = marginToP ;
+					var thisPoint = hash.tree[_id] || point ;
+					thisPoint.left = maxWidth/2 ;
+					thisPoint.top = marginToP ;
 					var t_index = hash.headId.indexOf(_id) ;
-					if(t_index>0){ 
-						point.thispoint.top = marginToP + hash.tree[hash.headId[t_index-1]].height + hash.tree[hash.headId[t_index-1]].top ;
+					if(t_index!=-1){ 
+						var prevHeight =0, prevTop = 0 ;
+						if(t_index!=0){
+							prevHeight = hash.tree[hash.headId[t_index-1]].height ;
+							prevTop = hash.tree[hash.headId[t_index-1]].top ;
+						}
+						thisPoint.top = marginToP + prevHeight + prevTop ;
+					}else{
+						hash.headId.push(_id) // 记录顶点
 					}
-					hash.tree[_id] = point.thispoint ;
-					hash.headId.push(_id) // 记录顶点
+					hash.tree[_id] = thisPoint ;
 					hash.readyId.push(_id) // 此点完成
 				}else{ // 非顶点
-					point.parent = getParentPoint(_parentId);
-					if(point.parent){
-						point.thispoint.top = point.parent.top + marginToP ;
-						addParentHeight(point.thispoint)
-						hash.tree[_id] = point.thispoint ;
+					var parent = getTreePoint(_parentId);
+					if(parent){
+						if(parent.sonIds.indexOf(_id)==-1){parent.sonIds.push(_id)};
+						point.top = parent.top + marginToP ;
+						
+						addParentHeight(point);
+						addParentWidth(point);
+						
+						hash.tree[_id] = point ;
+						
+						point.left = getThisLeft(point);
 						hash.readyId.push(_id) // 此点完成
 					}
 				}
-				
-				treeData.push(point);
 			})
 			
-			function getParentPoint(id){ // 获取父级节点
-				for(i in hash.tree){
-					if(i==id){
-						return hash.tree[i];
-					}
-				}
-				return null ;
+			
+			if(!testAllPointReady()){ 
+				getPartPoint();
+			}else {
+				//console.log(hash)
+				drawTree(hash);
 			}
 			
-			function addParentHeight(point){ // 递归成长父级高度
+			function getTreePoint(id){ // 获取父级节点
+				return hash.tree[id] || null ;
+			}
+			
+			function addParentHeight(point){ // 成长父级高度
 				if(point.parentId){
 					var parentPoint = hash.tree[point.parentId] ;
 					if(!parentPoint.heightAdded){
@@ -84,11 +95,28 @@ window.addEventListener("load",function(){
 				}
 			}
 			
-			if(!testAllPointReady()){ 
-				getPartPoint();
-			}else {
-				drawTree(treeData,hash) ;
+			function getThisLeft(point){
+				//console.log(point)
+				
+				return 500;
+				
 			}
+			
+			function addParentWidth(point){  // 成长父级宽度
+				if(point&&point.parentId){
+					var pointParent = hash.tree[point.parentId] ;
+					
+					for(var i=0;i< pointParent.sonIds.length;i++){
+						var id = pointParent.sonIds[i]
+						console.log(point.id,pointParent.sonIds,id)
+						//console.log(hash.tree[id])
+					}
+					
+					addParentWidth(hash.tree[pointParent.parentId]);
+				}
+			}
+			
+			
 		}
 		
 		function testAllPointReady(){  // 确认所有点计算完毕
@@ -110,10 +138,10 @@ window.addEventListener("load",function(){
 	
 	
 	
-	function  drawTree(data,hash){ // 开始绘制
-		console.table(data);
-		
+	function  drawTree(hash){ // 开始绘制
+		//console.log(hash)
 		var lastPointId = hash.headId[hash.headId.length-1] ;
+		
 		var offsetH = hash.tree[lastPointId].top + hash.tree[lastPointId].height ;
 		var c = document.getElementById("yun");
 		c.height = offsetH 
@@ -123,9 +151,10 @@ window.addEventListener("load",function(){
 		ctx.fillStyle="#fff";
 		ctx.fillRect(0,0,1000,offsetH); 
 		
-		data.forEach(function(point,index){
-			writeText(point.thispoint)
-		})
+		for(point in hash.tree){
+			//console.log(22222,hash.tree[point])
+			writeText(hash.tree[point])
+		}
 		
 		function writeText(point){
 			ctx.font="30px Arial";
